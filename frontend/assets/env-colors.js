134fa541b3115347f7c93b0f124bfac493d6f1c4
siron-1997 <sironvssiron@gmail.com>
2023-04-12 19:00:08 +0900

    adds env colors and mountain colors

---
+++ frontend/assets/env-colors.js
@@ -0,0 +1,28 @@
+export const envColors = {
+    evening: {
+        fog: '#67517D',
+        clearSky: '#C575D2',
+        thinCloud: '#B45DD1',
+        thickCloud: '#9E81C2',
+        background: 'linear-gradient(0deg, rgba(212, 139, 0, 1) 28%, rgba(174, 21, 58, 1) 61%, rgba(68, 4, 116, 1) 84%, rgba(43, 7, 110, 1) 91%, rgba(1, 3, 93, 1) 99%)',
+        environment: '#916CB5'
+    },
+    night: {
+        fog: '#3E106F',
+        clearSky: '#6943A9',
+        thinCloud: '#6943A9',
+        thickCloud: '#6943A9',
+        background: 'linear-gradient(180deg, rgba(26,3,74,1) 0%, rgba(61,118,170,1) 0%, rgba(90,67,142,1) 25%, rgba(48,7,87,1) 50%',
+        environment: '#3F226A'
+    },
+    lunch: {
+        fog: '#8DA1AA',
+        clearSky: '#ABCEE7',
+        thinCloud: '#8DAED2',
+        thickCloud: '#849BA9',
+        background: 'linear-gradient(0deg, rgba(116, 201, 246, 1) 8%, rgba(73, 160, 255, 1) 47%, rgba(60, 121, 255, 1) 90%)',
+        environment: '#8DA1AA'
+    }
+}
+
+// #341E55
