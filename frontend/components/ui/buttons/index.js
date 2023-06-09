12ac3c94998587c6973f137d980d04108293d2ff
siron-1997 <sironvssiron@gmail.com>
2023-06-09 11:45:27 +0900

    add toggle button

--- frontend/components/ui/buttons/index.js
+++ frontend/components/ui/buttons/index.js
@@ -1,3 +1,4 @@
 export { Close } from './close'
 export { Hamburger } from './hamburger'
 export { ScrollToTopButton } from './scrollToTopButton'
+export { ToggleButton } from './toggleButton'
---
+++ frontend/components/ui/buttons/toggleButton/ToggleButton.js
@@ -0,0 +1,60 @@
+import { useEffect, useRef, useContext } from 'react'
+import { Typography } from '@mui/material'
+import cn from 'classnames'
+import { WorkDataContext } from '@/pages/works/[work]'
+import { toggleButtonAnimation } from '@/animations/components/ui/buttons'
+import s from '@/styles/ui/button.module.css'
+
+export default function ToggleButton() {
+    const bgRef = useRef(null)
+    const { setIsFingerVisible, isViewerActive, setIsViewerActive } = useContext(WorkDataContext)
+
+    const textStyle = {
+        display: 'block',
+        position: 'relative',
+        fontSize: 20,
+        lineHeight: 2,
+        width: 130,
+        height: 45,
+        transition: 'all 0.25s'
+    }
+
+    const leftButtonClassNames = cn(s.toggle_button, s.left),
+          rightButtonClassNames = cn(s.toggle_button, s.right)
+
+    const handleClick = bool => {
+        setIsFingerVisible(bool)
+        setIsViewerActive(bool)
+    }
+
+    useEffect(() => {
+        const bg = bgRef.current
+        const cleanup = toggleButtonAnimation(bg, isViewerActive)
+
+        return () => cleanup()
+    }, [isViewerActive])
+
+    return (
+        <div className={s.toggle} style={{ marginTop: setIsViewerActive ? 'auto' : '0' }}>
+            <div className={s.bg} ref={bgRef} />
+            <Typography component='div' className={leftButtonClassNames}>
+                <Typography
+                    component='span'
+                    sx={textStyle}
+                    onClick={() => handleClick(true)}
+                >
+                    Start
+                </Typography>
+            </Typography>
+            <Typography component='div' className={rightButtonClassNames}>
+                <Typography
+                    component='span'
+                    sx={textStyle}
+                    onClick={() => handleClick(false)}
+                >
+                    End
+                </Typography>
+            </Typography>   
+        </div>
+    )
+}
