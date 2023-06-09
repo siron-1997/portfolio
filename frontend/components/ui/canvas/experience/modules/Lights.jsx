a23d4801b6c0381065871e3b261e666282985c9c
siron-1997 <sironvssiron@gmail.com>
2023-06-09 11:50:44 +0900

    delete files

--- frontend/components/ui/canvas/experience/modules/Lights.jsx
+++
@@ -1,8 +0,0 @@
-export default function Lights() {
-    return (
-        <>
-            <ambientLight color={'white'} intensity={0} />
-            <directionalLight color={'white'} intensity={1.0} position={[- 50, 300, -80]} />
-        </>
-    )
-}
