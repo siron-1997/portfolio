495f6a5fc97ad41635ff2b616f54e6f68a814295
siron-1997 <sironvssiron@gmail.com>
2023-06-22 00:06:08 +0900

    add assets

--- frontend/assets/env-colors.js
+++ frontend/assets/env-colors.js
@@ -2,16 +2,16 @@ export const envColors = {
     evening: {
         fog: 'rgb(125, 87, 124)',
         clearSky:  'rgb(170, 88, 170)',
-        thinCloud: 'rgb(180, 93, 209)',
-        thickCloud: 'rgb(131, 113, 157)',
+        thinCloud: 'rgb(190, 93, 189)',
+        thickCloud: 'rgb(125, 113, 157)',
         background: 'linear-gradient(180deg, rgba(99,72,198,1) 0%, rgba(110,46,166,1) 7%, rgba(227,73,121,1) 32%, rgba(241,145,66,1) 50%)',
-        environment: 'rgb(155, 42, 192)'
+        environment: 'rgb(170, 85, 175)'
     },
     night: {
         fog: 'rgb(52, 30, 85)',
         clearSky: 'rgb(105, 67, 169)',
         thinCloud: 'rgb(105, 67, 169)',
-        thickCloud: 'rgb(105, 67, 169)',
+        thickCloud: 'rgb(115, 77, 169)',
         background: 'linear-gradient(180deg, rgba(26,3,74,1) 0%, rgba(61,118,170,1) 0%, rgba(90,67,142,1) 25%, rgba(48,7,87,1) 50%',
         environment: 'rgb(77, 32, 109)'
     },
