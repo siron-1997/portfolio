34a7359caafc739f49c89b4d8beda51c8dd7df11
siron-1997 <sironvssiron@gmail.com>
2023-06-21 23:55:26 +0900

    add assets

---
+++ frontend/assets/sns-list.js
@@ -0,0 +1,20 @@
+const path = '/icons/',
+      svg = '.svg'
+
+export const snsList = [
+    {
+        image: path + 'twitter_sns_48x48' + svg,
+        alt: 'twitter',
+        link: 'https://twitter.com/Jsiron2029'
+    },
+    {
+        image: path + 'instagram_sns_48x48' + svg,
+        alt: 'instagram',
+        link: 'https://www.instagram.com/shiron50'
+    },
+    {
+        image: path + 'github_sns_60x60' + svg,
+        alt: 'github',
+        link: 'https://github.com/siron-1997'
+    }
+]
